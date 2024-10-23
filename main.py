e6e7042d445cfd633b3fa4fd7d7ca161785afa08
Ivan <119617511+ivantw829@users.noreply.github.com>
2024-10-23 15:38:13 +0800

    1.1.0 修復搶頭香錯誤

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ async def 簽到天數(
     cursor_first = conn_first.cursor()
     # 如果 user_id 不在裡面就創建一條
     cursor_first.execute(
-        f'CREATE TABLE IF NOT EXISTS "{user_id}" (date TEXT PRIMARY KEY)'
+        f'CREATE TABLE IF NOT EXISTS "{user_id}" (day TEXT PRIMARY KEY)'
     )
     conn_first.commit()
     cursor_first.execute(f"SELECT * FROM '{user_id}'")
